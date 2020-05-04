a1db205b9a612711d6d2815f0d30c0aea916b180
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2020-05-04 10:29:37 -0300

    fix(batch-size): added to inputs on brnn

--- ctc/models/__init__.py
+++ ctc/models/__init__.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from models.brnn_model import brnn_model
 
-brnn = brnn_model(512)
+brnn = brnn_model(512, batch_size=4)
 loss = {'ctc': lambda y_true, y_pred: y_pred}
 optimizer = tf.keras.optimizers.Adam(
     lr=0.0001,
